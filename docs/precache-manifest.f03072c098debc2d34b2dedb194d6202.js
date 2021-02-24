245c0230a15d32d574e40dfa6a892d7468208580
Josh Hanson <joshhazn@gmail.com>
2021-02-23 20:27:32 -0500

    Update build to docs

---
+++ docs/precache-manifest.f03072c098debc2d34b2dedb194d6202.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc760be164d5317dadb2980b43fbc17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67e8524aaae4cb42bb95",
+    "url": "/static/css/main.5c105888.chunk.css"
+  },
+  {
+    "revision": "9e772782e406551189c5",
+    "url": "/static/js/2.1e7e2c69.chunk.js"
+  },
+  {
+    "revision": "118c852e3a2eecb4d5d932d651be591e",
+    "url": "/static/js/2.1e7e2c69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67e8524aaae4cb42bb95",
+    "url": "/static/js/main.7f9e9c82.chunk.js"
+  },
+  {
+    "revision": "9846775bccce59f39b31",
+    "url": "/static/js/runtime-main.5674c429.js"
+  },
+  {
+    "revision": "2d430caf9a9d2dcee0b14f21e8310b8f",
+    "url": "/static/media/CentreCircle.2d430caf.png"
+  },
+  {
+    "revision": "3ce6f92383f57e08c99b8f6f1453887e",
+    "url": "/static/media/Island_Games_logo.3ce6f923.png"
+  },
+  {
+    "revision": "5aec06e040f5f00cf62ec7b7bcdf5304",
+    "url": "/static/media/StatsPerform.5aec06e0.png"
+  },
+  {
+    "revision": "4b0bd202840ec1f75d7d19140248f01c",
+    "url": "/static/media/blank-icon.4b0bd202.png"
+  }
+]);
