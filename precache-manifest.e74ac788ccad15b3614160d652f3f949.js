e82921d3d8ddaba53eb40acf99950998f80ad30c
Josh Hanson <joshhazn@gmail.com>
2021-02-23 20:43:27 -0500

    Updates

---
+++ precache-manifest.e74ac788ccad15b3614160d652f3f949.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e193c0a54f95068938520ebc5bab462",
+    "url": "/canpl/index.html"
+  },
+  {
+    "revision": "8220c89ee7aef3131bff",
+    "url": "/canpl/static/css/main.5c105888.chunk.css"
+  },
+  {
+    "revision": "adfc320ae8aefce26569",
+    "url": "/canpl/static/js/2.55b9e2ae.chunk.js"
+  },
+  {
+    "revision": "118c852e3a2eecb4d5d932d651be591e",
+    "url": "/canpl/static/js/2.55b9e2ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8220c89ee7aef3131bff",
+    "url": "/canpl/static/js/main.e3255ec5.chunk.js"
+  },
+  {
+    "revision": "26f8345179f10e25779e",
+    "url": "/canpl/static/js/runtime-main.a88e4178.js"
+  },
+  {
+    "revision": "2d430caf9a9d2dcee0b14f21e8310b8f",
+    "url": "/canpl/static/media/CentreCircle.2d430caf.png"
+  },
+  {
+    "revision": "3ce6f92383f57e08c99b8f6f1453887e",
+    "url": "/canpl/static/media/Island_Games_logo.3ce6f923.png"
+  },
+  {
+    "revision": "5aec06e040f5f00cf62ec7b7bcdf5304",
+    "url": "/canpl/static/media/StatsPerform.5aec06e0.png"
+  },
+  {
+    "revision": "4b0bd202840ec1f75d7d19140248f01c",
+    "url": "/canpl/static/media/blank-icon.4b0bd202.png"
+  }
+]);
